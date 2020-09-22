f63016186e03f6c7ff095480d81b7bbb83edbc78
Mike Boisvert <mdboisve@mtu.edu>
2020-09-22 18:24:08 -0400

    Adding one line in startup.cs for ease of use.

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace ChemStoreWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
