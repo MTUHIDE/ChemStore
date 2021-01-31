8db52ad3573daf310c6e2ae93c4ae7792df90f1d
jeholtre <jeholtre@mtu.edu>
2021-01-31 16:52:22 -0500

    Fixed database migrations and added PIC_ID fk for container table

---
+++ Migrations/20210131213859_AddPicForeignKey.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChemStoreWebApp.Migrations
+{
+    public partial class AddPicForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PicId",
+                table: "container",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PicId",
+                table: "container");
+        }
+    }
+}
