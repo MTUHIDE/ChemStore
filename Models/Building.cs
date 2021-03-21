df89786208dcd4d92622cacdf774aef58aeca88f
jhoff2358 <35403942+jhoff2358@users.noreply.github.com>
2021-03-21 14:17:36 -0400

    Enums (#11)
    * Add Enums and functionality for dropdowns
    
    Changed building, department, and units to enums.
    Added dropdown functionality for buildings, departments, and units
    
    * Revert "Add Enums and functionality for dropdowns"
    
    This reverts commit 08c9d4cedd16ba1ee40bb0d50878f02f1af5f9b6.
    
    * Revert "Revert "Add Enums and functionality for dropdowns""
    
    This reverts commit eff51fd84f48c80892d3f5f8f7fcf247aed2e05f.
    
    * Jason forgets how to use github
    
    i unscrewed it up

--- Models/Building.cs
+++ Models/Building.cs
@@ -1,11 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 
+public enum Buildings
+{
+    AdminBuilding,
+    LakeshoreCenter,
+    ROTC,
+    AOB,
+    EERC,
+    Dow,
+    AlumniHouse,
+    Rozsa,
+    Walker,
+    MandM,
+    Hamar,
+    Dillman,
+    Fisher,
+    WidmaierHouse,
+    Library,
+    Forestry,
+    ChemSci,
+    MEEM,
+    SDC,
+    ShermanFieldPressBox,
+    FordCenter,
+    Rekhi,
+    LittleHuskies,
+    DHH,
+    DaniellHeights,
+    MUB,
+    Wads,
+    WestMcNair,
+    EastMcNair,
+    CentralHeatingPlant,
+    LakesideLabratory,
+    Facilities,
+    Hillside,
+    Gates,
+    KRC,
+    Meese,
+    ATDC,
+    Museum,
+    GLRC
+}
+
 namespace ChemStoreWebApp.Models
 {
     public partial class Building
     {
         public int BuildingId { get; set; }
-        public string BuildingName { get; set; }
+        public Buildings BuildingName { get; set; }
     }
 }
