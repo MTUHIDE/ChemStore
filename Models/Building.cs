8f6e9ddf3bb1edca2ac92a7c32896ee51e81f34e
jhoff2358 <35403942+jhoff2358@users.noreply.github.com>
2021-04-14 00:22:02 -0400

    Enums (#13)
    * Add Enums and functionality for dropdowns
    
    Changed building, department, and units to enums.
    Added dropdown functionality for buildings, departments, and units
    
    * Revert "Add Enums and functionality for dropdowns"
    
    This reverts commit 08c9d4cedd16ba1ee40bb0d50878f02f1af5f9b6.
    
    * Revert "Revert "Add Enums and functionality for dropdowns""
    
    This reverts commit eff51fd84f48c80892d3f5f8f7fcf247aed2e05f.
    
    * Jason forgets how to use github
    
    i unscrewed it up
    
    * Added retired label to container
    
    * Added English names to enums

--- Models/Building.cs
+++ Models/Building.cs
@@ -1,46 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public enum Buildings
 {
+    [Display(Name = "Administration Building")]
     AdminBuilding,
+    [Display(Name = "Lakeshore Center")]
     LakeshoreCenter,
+    [Display(Name = "ROTC Building")]
     ROTC,
+    [Display(Name = "Academic Office Building")]
     AOB,
+    [Display(Name = "Electrical Engineering Resources Center")]
     EERC,
+    [Display(Name = "Dow Environmental Sciences and Engineering Building")]
     Dow,
+    [Display(Name = "Alumni House")]
     AlumniHouse,
+    [Display(Name = "Rozsa Center for the Performing Arts")]
     Rozsa,
+    [Display(Name = "Walker Arts and Humanities Center")]
     Walker,
+    [Display(Name = "Minerals and Materials Engineering Building")]
     MandM,
+    [Display(Name = "Hamar House")]
     Hamar,
+    [Display(Name = "Grover C Dillman Hall")]
     Dillman,
+    [Display(Name = "Fisher Hall")]
     Fisher,
+    [Display(Name = "Widmaier House")]
     WidmaierHouse,
+    [Display(Name = "J. R. Van Pelt and John and Ruanne Opie Library")]
     Library,
+    [Display(Name = "U. J. Noblet Forestry Building")]
     Forestry,
+    [Display(Name = "Chemical Sciences and Engineering Building")]
     ChemSci,
+    [Display(Name = "R. L. Smith Building")]
     MEEM,
+    [Display(Name = "Student Development Complex")]
     SDC,
+    [Display(Name = "Sherman Field Press Box")]
     ShermanFieldPressBox,
+    [Display(Name = "Ford Center")]
     FordCenter,
+    [Display(Name = "Kanwal and Ann Rekhi Hall")]
     Rekhi,
+    [Display(Name = "Little Huskies Child Development Center")]
     LittleHuskies,
+    [Display(Name = "Douglass Houghton Hall")]
     DHH,
+    [Display(Name = "Daniell Heights Apartments")]
     DaniellHeights,
+    [Display(Name = "Memorial Union Building")]
     MUB,
+    [Display(Name = "Wadsworth Hall")]
     Wads,
+    [Display(Name = "West McNair Hall")]
     WestMcNair,
+    [Display(Name = "East McNair Hall")]
     EastMcNair,
+    [Display(Name = "Central Heating Plant")]
     CentralHeatingPlant,
+    [Display(Name = "Lakeside Labratory")]
     LakesideLabratory,
+    [Display(Name = "Storage-Service")]
     Facilities,
+    [Display(Name = "Hillside Place")]
     Hillside,
+    [Display(Name = "Gates Tennis Center")]
     Gates,
+    [Display(Name = "Keweenaw Research Center")]
     KRC,
+    [Display(Name = "Harold Meese Center")]
     Meese,
+    [Display(Name = "Advanced Technology Development Complex")]
     ATDC,
+    [Display(Name = "A. E. Seaman Mineral Museum")]
     Museum,
+    [Display(Name = "Great Lakes Research Center")]
     GLRC
 }
 
