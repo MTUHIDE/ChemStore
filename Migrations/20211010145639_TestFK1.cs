cf5b225f90e3e8a3916e624bbf1ca56d2e6e149b
Jason Hoffman <jhoff2358@gmail.com>
2021-10-10 11:34:30 -0400

    Got DisplayContainer working again so all fields show on search

---
+++ Migrations/20211010145639_TestFK1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace ChemStoreWebApp.Migrations
+{
+    public partial class TestFK1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Supervisor_ID",
+                table: "container",
+                nullable: false,
+                comment: "FK(Supervisor.ID)",
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldComment: "FK(Supervisor.ID)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Account_ID",
+                table: "account",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Supervisor_ID",
+                table: "container",
+                type: "bigint",
+                nullable: false,
+                comment: "FK(Supervisor.ID)",
+                oldClrType: typeof(int),
+                oldComment: "FK(Supervisor.ID)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Account_ID",
+                table: "account",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -19,7 +19,7 @@ namespace ChemStoreWebApp.Pages
         {
             _context = context;
         }
-        public IList<Container> Containers { get; set; }
+        public IList<DisplayContainer> DisplayContainers { get; set; }
 
         // variables bound to the URL storing search terms
         [BindProperty(SupportsGet = true)]
@@ -59,26 +59,25 @@ namespace ChemStoreWebApp.Pages
         /// </summary>
         /// <param name="con">Container object</param>
         /// <returns>True if container should be listed</returns>
-        public Boolean isValidSearchItem(Container con)
-        {/*
-            if (!string.IsNullOrEmpty(searchCAS) && con.chem.CasNumber != Int32.Parse(searchCAS))
+        public Boolean isValidSearchItem(DisplayContainer con, bool ignoreCase)
+        {
+            var checkCase = ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+            if (!string.IsNullOrEmpty(searchCAS) && !con.chem.CasNumber.Contains(searchString, checkCase))
                 return false;
-            if (!string.IsNullOrEmpty(searchString) && !con.chem.ChemName.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            if (!string.IsNullOrEmpty(searchString) && !con.chem.ChemicalName.Contains(searchString, checkCase))
                 return false;
-            if (!string.IsNullOrEmpty(searchBuilding) && !con.building.BuildingName.ToString().Equals(searchBuilding))
+            if (!string.IsNullOrEmpty(searchBuilding) && !con.loc.BuildingName.ToString().Equals(searchBuilding))
                 return false;
-            if (!string.IsNullOrEmpty(searchSize) && con.con.Size != Int32.Parse(searchSize))
+            if (!string.IsNullOrEmpty(searchSize) && con.con.Amount != Int32.Parse(searchSize))
                 return false;
-            if (!string.IsNullOrEmpty(searchEmail) && !con.pic.Email.Contains(searchEmail, StringComparison.OrdinalIgnoreCase))
+            if (!string.IsNullOrEmpty(searchEmail) && !con.supervisor.Email.Contains(searchEmail, checkCase))
                 return false;
             if (!string.IsNullOrEmpty(searchUnits) && !con.con.Unit.Equals((Units) Int32.Parse(searchUnits)))
                 return false;
-            if (!string.IsNullOrEmpty(searchDepartment) && !con.loc.Department.ToString().Equals(searchDepartment))
+            if (!string.IsNullOrEmpty(searchDepartment) && !con.supervisor.Department.ToString().Equals(searchDepartment))
                 return false;
             if (!string.IsNullOrEmpty(searchRetired) && bool.Parse(searchRetired) != con.con.Retired)
                 return false;
-            if (!string.IsNullOrEmpty(searchRetired) && bool.Parse(searchRetired) != con.con.Retired)
-                return false;*/
 
             return true;
         }
@@ -88,33 +87,39 @@ namespace ChemStoreWebApp.Pages
         /// <returns></returns>
         public async Task OnGetAsync()
         {
-            /*
-            var containers = from c in _context.Container select c;
-            bool ignoreCase = true; // temp variable to store whether or not case matters in search
-            var checkCase = ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+            // stores data from database in arrays to limit amount of calls to database at once
+            var containers = _context.Container.ToList();
+            var chemicals = _context.Chemical.ToList();
+            var accounts = _context.Account.ToList();
+            var locations = _context.Location.ToList();
+
+            DisplayContainers = await Task.FromResult(containers.Select( // creates a DisplayContainer object with all info needed to display it
+                c => new DisplayContainer(c, chemicals, locations, accounts))
+                .Where(c => isValidSearchItem(c, true))
+                .ToList());
 
             // Checks each search term against the current list of containers
-
+            /*
             if (!string.IsNullOrEmpty(searchCAS))
                 containers = containers.Where(c => c.CasNumber.Contains(searchCAS, checkCase));
             if (!string.IsNullOrEmpty(searchString))
                 containers = containers.Where(c => c.CasNumberNavigation.ChemicalName.Contains(searchString, checkCase));
-            if (!string.IsNullOrEmpty(searchBuilding))
-                containers = containers.Where(c => c.Room.BuildingName.Equals((Buildings)Int32.Parse(searchBuilding)));
-            if (!string.IsNullOrEmpty(searchSize))
-                containers = containers.Where(c => c.Amount.Equals(Int32.Parse(searchSize)));
-            if (!string.IsNullOrEmpty(searchEmail))
-                containers = containers.Where(c => c.Supervisor.Email.Contains(searchEmail, checkCase));
-            if (!string.IsNullOrEmpty(searchUnits))
-                containers = containers.Where(c => c.Unit.Equals((Units)Int32.Parse(searchUnits)));
-            if (!string.IsNullOrEmpty(searchDepartment))
-                containers = containers.Where(c => c.Supervisor.Department.Equals((Departments)Int32.Parse(searchDepartment)));
-            if (!string.IsNullOrEmpty(searchRetired))
-                containers = containers.Where(c => c.Retired == bool.Parse(searchRetired));
+            //if (!string.IsNullOrEmpty(searchBuilding))
+            //    containers = containers.Where(c => c.Room.BuildingName.Equals((Buildings)Int32.Parse(searchBuilding)));
+            //if (!string.IsNullOrEmpty(searchSize))
+            //    containers = containers.Where(c => c.Amount.Equals(Int32.Parse(searchSize)));
+            //if (!string.IsNullOrEmpty(searchEmail))
+            //    containers = containers.Where(c => c.Supervisor.Email.Contains(searchEmail, checkCase));
+            //if (!string.IsNullOrEmpty(searchUnits))
+            //    containers = containers.Where(c => c.Unit.Equals((Units)Int32.Parse(searchUnits)));
+            //if (!string.IsNullOrEmpty(searchDepartment))
+            //    containers = containers.Where(c => c.Supervisor.Department.Equals((Departments)Int32.Parse(searchDepartment)));
+            //if (!string.IsNullOrEmpty(searchRetired))
+            //   containers = containers.Where(c => c.Retired == bool.Parse(searchRetired));
 
-            Containers = await containers.ToListAsync();*/
+            Containers = await containers.ToListAsync();
 
-            Containers = await _context.Container.ToListAsync();
+            //Containers = await _context.Container.ToListAsync();*/
         }
     }
 }
