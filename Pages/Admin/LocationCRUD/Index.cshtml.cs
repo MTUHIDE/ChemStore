d422d0871c51c3596224547d3812faecddfaf053
Josh Overbeek <joshoverbeek0@gmail.com>
2022-09-23 11:56:06 -0400

    Created folder for the CRUD page and added blank index file

---
+++ Pages/Admin/LocationCRUD/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChemStoreWebApp.Pages.Admin.LocationCRUD
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
