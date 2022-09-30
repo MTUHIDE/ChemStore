2640baaa766e66d7dcef27a7dd5668b52239c3e8
btfuhrma <btfuhrma@mtu.edu>
2022-09-30 11:20:24 -0400

    Added code for create page and should save stuff

--- Pages/Admin/LocationCRUD/Create.cshtml.cs
+++ Pages/Admin/LocationCRUD/Create.cshtml.cs
@@ -6,9 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ChemStoreWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.CodeAnalysis;
+using Location = ChemStoreWebApp.Models.Location;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Framework.Profiler;
 
 namespace ChemStoreWebApp.Pages.Admin.LocationCRUD
 {
+    //[Authorize(Roles = "Admin")]
     public class CreateModel : PageModel
     {
         private readonly ChemStoreWebApp.Models.chemstoreContext _context;
@@ -25,19 +33,20 @@ namespace ChemStoreWebApp.Pages.Admin.LocationCRUD
 
         [BindProperty]
         public Location Location { get; set; }
-
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return Page();
+
+                String id = Guid.NewGuid().ToString();
+                Location.RoomId = id;
+                _context.Location.Add(Location);
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage("./Index");
             }
-
-            _context.Location.Add(Location);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            return Page();
         }
     }
 }
