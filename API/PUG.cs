2fe1e84983e8ba7bef1d15bb8c71d92ea38fc80b
SunOfLife1 <yfitzge@gmail.com>
2024-02-04 13:44:11 -0500

    Add GetSection
    Adds a method to find a specified section within a PUG View Compound.

--- API/PUG.cs
+++ API/PUG.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -42,11 +43,28 @@ namespace ChemStoreWebApp.PUG
             string src = $"data/compound/{CID}/JSON";
 
             string response = await client.GetStringAsync(src);
-            Console.WriteLine(response);
+            // Console.WriteLine(response);
+
+            JsonNode node = JsonNode.Parse(response);
+            Console.WriteLine($"Name: {node["Record"]["RecordTitle"]}\n");
+            GetSection(node["Record"]["Section"], "Names and Identifiers");
             //await using Stream stream = await client.GetStreamAsync(src);
             //var response = await JsonSerializer.DeserializeAsync<Dictionary<string, Dictionary<string, List<int>>>>(stream);
             // Console.WriteLine(response["IdentifierList"]["CID"].Count);
             //return response["IdentifierList"]["CID"];
+
+            return null;
+        }
+
+        private static JsonNode GetSection(JsonNode node, string sectionHeading)
+        {
+            JsonArray arr = node.AsArray();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine($"{i}: {arr[i]["TOCHeading"]}");
+                //Console.WriteLine($"{arr[i]["TOCHeading"]}");
+                if (arr[i]["TOCHeading"].Deserialize<string>() == sectionHeading) return arr[i];
+            }
 
             return null;
         }
