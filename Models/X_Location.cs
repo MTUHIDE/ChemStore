1728af3e42da9b993d15c02b8c68766b320d55fd
noah-yac <93050917+noah-yac@users.noreply.github.com>
2024-09-22 14:44:01 -0400

    Removed old Location model, updated new model

--- Models/X_Location.cs
+++ Models/X_Location.cs
@@ -6,26 +6,23 @@ using System.Numerics;
 
 namespace ChemStoreWebApp.Models
 {
-
-
     public partial class X_Location
     {
         [Key]
-        public int LocationID { get; set; }
-        public int Level { get; set; }
-        public string Name { get; set; }
-        public bool IsHidden { get; set; }
-
-        [ForeignKey(nameof(X_Location_2))]
-        public int ParentID { get; set; }
-        public X_Location X_Location_2 { get; set; }
+        public int LocationID { get; set; } //similar to RoomID (this says smallint on database, but we're likely going to refactor with guid/uuid)
+        public short ParentID { get; set; } //similar to RoomNumber, BuildingName
+        public byte Level { get; set; } //similar to RoomNumber, BuildingName
+        public string Name { get; set; } //similar to RoomNumber, BuildingName
 
         [ForeignKey(nameof(Department))]
-        public int DepartmentID { get; set; }
-        public Department Department { get; init; }
+        public int DepartmentID { get; set; } //this says tinyInt on database, left as int for now
+        public Department Department { get; init; } 
 
-        /*[ForeignKey(nameof(User))]
-        public int SupervisorID { get; set; }
-        public User User { get; init; }*/
+        [ForeignKey(nameof(User))]
+        public int SupervisorID { get; set; } //this says mediumint?, left as int for now
+        public User User { get; init; }
+
+        public bool IsHidden { get; set; }
+
     }
 }
