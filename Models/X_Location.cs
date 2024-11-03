7bc4a3ca8b33e551cc344a6ad52de8e3e461477d
SunOfLife1 <yfitzge@gmail.com>
2024-11-03 15:03:28 -0500

    Add new migration for Nullable Location ParentID

--- Models/X_Location.cs
+++ Models/X_Location.cs
@@ -10,7 +10,7 @@ namespace ChemStoreWebApp.Models
     {
         [Key]
         public int LocationID { get; set; } //similar to RoomID (this says smallint on database, but we're likely going to refactor with guid/uuid)
-        public short ParentID { get; set; } //similar to RoomNumber, BuildingName
+        public short? ParentID { get; set; } //similar to RoomNumber, BuildingName
         public byte Level { get; set; } //similar to RoomNumber, BuildingName
         public string Name { get; set; } //similar to RoomNumber, BuildingName
 
