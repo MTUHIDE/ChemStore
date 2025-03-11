d5a15081accc802d615c62968509ffd95a223391
SunOfLife1 <yfitzge@gmail.com>
2025-03-11 13:08:48 -0400

    Add extra line breaks to store/views.py
    I don't want to keep fighting my formatter here

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def index(request):
     # Everything in the dictionary can be referenced by the template
     data = {
@@ -7,21 +8,26 @@ def index(request):
     }
     return render(request, "store/index.html", data)
 
+
 def log(request):
     data = {}
     return render(request, "store/log.html", data)
+
 
 def contact(request):
     data = {}
     return render(request, "store/contact.html", data)
 
+
 def admin(request):
     data = {}
     return render(request, "store/admin.html", data)
 
+
 def debug(request):
     data = {}
     return render(request, "store/debug.html", data)
+
 
 def privacy(request):
     data = {}
