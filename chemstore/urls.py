26780362ce9dbcf2651a9238cb87b212193b6714
SunOfLife1 <yfitzge@gmail.com>
2025-03-24 12:19:29 -0400

    Un-bring back the Django Admin page

--- chemstore/urls.py
+++ chemstore/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django_admin/", admin.site.urls),
     path("", include("store.urls")),
 ]
