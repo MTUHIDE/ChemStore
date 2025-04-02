a9ac79dfa02edd308d11815dc3f18f0f979534a6
Rayun <nick@rayun.dev>
2025-04-02 01:03:58 -0400

    Allow ASGI (async) hosting with ServeStatic

--- chemstore/asgi.py
+++ chemstore/asgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from servestatic import ServeStaticASGI
 from django.core.asgi import get_asgi_application
+
+from chemstore.settings import STATIC_ROOT, STATIC_URL
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chemstore.settings")
 
 application = get_asgi_application()
+application = ServeStaticASGI(application, root=STATIC_ROOT, prefix=STATIC_URL)
