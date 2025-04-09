b04967cd6ad46614e1b045b8ab4ab96fd2074faa
Rayun <nick@rayun.dev>
2025-04-09 02:11:01 -0400

    Attempt #2

--- store/middleware/LoginRequired.py
+++ store/middleware/LoginRequired.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class LoginRequiredMiddleware:
@@ -7,7 +8,11 @@ class LoginRequiredMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not settings.DEBUG and request.path != settings.LOGIN_URL and not request.user.is_authenticated:
+        if (not settings.DEBUG
+                and request.path != '/accounts/login/'
+                and request.path != '/accounts/cas-login/'
+                and request.path != '/accounts/logout/'
+                and not request.user.is_authenticated):
             return redirect(f"{settings.LOGIN_URL}?next={request.path}")
         response = self.get_response(request)
         return response
