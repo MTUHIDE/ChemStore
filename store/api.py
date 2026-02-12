bdb64ae79fefe80920e5f0d6f5eea4d2e296d366
icyang1 <icyang@mtu.edu>
2026-02-12 17:52:06 -0500

    Update api.py

--- store/api.py
+++ store/api.py
@@ -5,3 +5,37 @@
 # CID list from PUG REST
 # Another function should take in a CID and return some info about the compound
 # from PUG View
+import requests
+
+BASE = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
+PROPS = "MolecularWeight,MolecularFormula,IsomericSMILES,IUPACName"
+
+name = input("Enter Element or compound: ")
+
+
+def get_pubchem_info(name):
+    cid_url = f"{BASE}/compound/name/{name}/cids/JSON"
+    cid_response = requests.get(cid_url)
+
+    if cid_response.status_code != 200:
+        
+        return f"Failed to find information on {name}"
+
+    cid_data = cid_response.json()
+    cids = cid_data.get("IdentifierList", {}).get("CID", [])
+
+    if not cids:
+        return f"Failed to find information on {name}"
+
+    cid = cids[0]
+
+    prop_url = f"{BASE}/compound/cid/{cid}/property/{PROPS}/JSON"
+    prop_response = requests.get(prop_url)
+
+    if prop_response.status_code != 200:
+        return f"Failed to find information on {name}"
+
+    data = prop_response.json()
+    return data["PropertyTable"]["Properties"][0]
+
+print(get_pubchem_info(name))
