5c0d8d1eaf57d70287f0f39e82d9eb5886addb65
SunOfLife1 <sunoflife1.git@gmail.com>
2026-02-12 17:49:00 -0500

    Create api.py file for interacting with the APIs

---
+++ store/api.py
@@ -0,0 +1,7 @@
+# This file will contain functions to interact with the PubChem PUG REST and PUG
+# View APIs. Interacting with the two APIs should be split into different
+# functions
+# For example, one function should take in a string (name of compound) to get a
+# CID list from PUG REST
+# Another function should take in a CID and return some info about the compound
+# from PUG View
